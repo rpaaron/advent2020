55407ee95e71c895826575045a34310903fb61cb
Aaron Lipinski <aaron.lipinski@roboticsplus.co.nz>
2020-12-15 12:30:59 +1300

    Day 13 part 2

--- 13.mjs
+++ 13.mjs
@@ -4,7 +4,7 @@ const lines = fs.readFileSync('13.input.txt').toString().split("\n")
 	.filter(line => line.length > 0)
 
 const earliestDepature = lines[0] * 1
-const buses = lines[1].split(',').filter(x => x * 1).map(x => x * 1)
+const buses = lines[1].split(',').map(x => x * 1)
 
 console.log({earliestDepature})
 console.log({buses})
@@ -29,3 +29,27 @@ const leBus = Object.entries(map).reduce(
 
 console.log({leBus})
 console.log('Part1', leBus.bus * leBus.distance)
+
+
+function rec(time, pos, every) {
+	while (true) {
+		const bus = buses[pos]
+		console.log({time, bus, pos, every})
+		if (isNaN(bus) || ((time + pos) % bus == 0)) { 
+			if (pos == buses.length -1)
+				return time;
+			return rec(time, pos+1, (bus||1) * every)
+		}
+
+		time+= every
+	}
+}
+
+const blah = rec(1, 0, 1)
+console.log('Part2', blah)
+//1111111111111111111111111111111
+//2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+//3  3  3  3  3  3  3  3  3  3  3
+//4   4   4   4   4   4   4   4   
+//5    5    5    5    5    5    5
+//6     6     6     6      6     6
