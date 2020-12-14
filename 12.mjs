65ae8003b7b25f3fb265fb8914f52fd35ea63c4b
Aaron Lipinski <aaron.lipinski@roboticsplus.co.nz>
2020-12-15 09:12:53 +1300

    Day 12 part 2

--- 12.mjs
+++ 12.mjs
@@ -60,6 +60,61 @@ const ans = lines.reduce((memo, cmd) => {
 
 }, {dir: 0, v: 0, h: 0})
 
-
 console.log({ans})
 console.log('Part1', Math.abs(ans.v) + Math.abs(ans.h))
+
+const p2 = lines.reduce((memo, cmd) => {
+	var o = cmd.o
+	switch(cmd.cmd) {
+		case 'F':
+			memo.ship.x += memo.h * cmd.o
+			memo.ship.y += memo.v * cmd.o
+		break;
+
+		case 'N':
+		memo.v += cmd.o
+		break;
+
+		case 'S':
+		memo.v -= cmd.o
+		break;
+
+		case 'E':
+		memo.h += cmd.o
+		break;
+
+		case 'W':
+		memo.h -= cmd.o
+		break;
+
+		case 'L':
+			// x  5  y +1
+			// x -1  y +5
+			// x -5  y -1
+			// x  1  y -5
+			while((o -= 90) >= 0) {
+				//console.log("hi", cmd.o)
+				const x = memo.h
+				const y = memo.v
+
+				memo.h = -y
+				memo.v = x
+			}
+
+		case 'R':
+			while((o -= 90) >= 0) {
+				const x = memo.h
+				const y = memo.v
+
+				memo.h = y
+				memo.v = -x
+			}
+		break;
+	}
+
+	console.log({cmd, memo})
+	return memo
+
+}, {v: 1, h: 10, ship: {x:0, y:0}})
+
+console.log('Part2', Math.abs(p2.ship.x) + Math.abs(p2.ship.y))
