1e227879197d6b948f13388e56ec3d5467618858
Aaron Lipinski <aaron.lipinski@roboticsplus.co.nz>
2020-12-14 17:46:45 +1300

    day 11 part 2

--- 11.mjs
+++ 11.mjs
@@ -10,34 +10,60 @@ const print = (layout) => console.log(layout.map(row => row.join('')).join('\n')
 const getPos = (layout, row, col) =>
 	layout[row] || []
 
-const seatCycle = (layout, row, col) =>
-	(layout[row-1] || [])[col-1] == '#' ||
-	(layout[row-1] || [])[col  ] == '#' ||
-	(layout[row-1] || [])[col+1] == '#' ||
-	(layout[row  ] || [])[col-1] == '#' ||
-	(layout[row  ] || [])[col+1] == '#' ||
-	(layout[row+1] || [])[col-1] == '#' ||
-	(layout[row+1] || [])[col  ] == '#' ||
-	(layout[row+1] || [])[col+1] == '#' ? 'L' : '#'
+const findSeat = (layout, row, col, row_dir, col_dir, depth) => {
+	//if (depth != 100000) throw "oope"
+	while(row >= 0 && row < layout.length &&
+	      col >= 0 && col < layout[0].length && depth > 0) {
+		depth--
+		if (depth <= 0) {
+			//console.log({row, col, depth, row_dir, col_dir})
+			//throw "depth expired"
+		}
 
-const emptyCycle = (layout, row, col) =>
-	(((layout[row-1] || [])[col-1] == '#') +
-	 ((layout[row-1] || [])[col  ] == '#') +
-	 ((layout[row-1] || [])[col+1] == '#') +
-	 ((layout[row  ] || [])[col-1] == '#') +
-	 ((layout[row  ] || [])[col+1] == '#') +
-	 ((layout[row+1] || [])[col-1] == '#') +
-	 ((layout[row+1] || [])[col  ] == '#') +
-	 ((layout[row+1] || [])[col+1] == '#')) > 3 ? 'L' : '#'
+		row+=row_dir
+		col+=col_dir
+		if ((layout[row]||[])[col] == '#') return true
+		if ((layout[row]||[])[col] == 'L') return false
+	}
+	return false
+}
 
-const cycle = (layout) => {
+const seatCycle = (layout, depth, row, col) => {
+	const found = (
+	findSeat(layout, row, col, -1, -1, depth) +
+	findSeat(layout, row, col, -1,  0, depth) + 
+	findSeat(layout, row, col, -1, +1, depth) + 
+	findSeat(layout, row, col,  0, -1, depth) + 
+	findSeat(layout, row, col,  0, +1, depth) + 
+	findSeat(layout, row, col, +1, -1, depth) + 
+	findSeat(layout, row, col, +1,  0, depth) + 
+	findSeat(layout, row, col, +1, +1, depth))
+	//console.log({row, col, found})
+	return found > 0 ? 'L' : '#'
+}
+
+const emptyCycle = (layout, depth, count, row, col) => {
+	const found = (
+	 findSeat(layout, row, col, -1, -1, depth) +
+	 findSeat(layout, row, col, -1,  0, depth) +
+	 findSeat(layout, row, col, -1, +1, depth) +
+	 findSeat(layout, row, col,  0, -1, depth) +
+	 findSeat(layout, row, col,  0, +1, depth) +
+	 findSeat(layout, row, col, +1, -1, depth) +
+	 findSeat(layout, row, col, +1,  0, depth) +
+	 findSeat(layout, row, col, +1, +1, depth))
+ 	 //console.log({row, col, found})
+	return found >= count ? 'L' : '#'
+}
+
+const cycle = (layout, depth, count) => {
 	var mutateCount = 0
 	const newLayout = layout.map(
 		(row, row_idx) => 
 			row.map((seat, col_idx) => {
 				const newSeat = 
-					seat === 'L' ? seatCycle(layout, row_idx, col_idx) :
-					seat === '#' ? emptyCycle(layout, row_idx, col_idx) :
+					seat === 'L' ? seatCycle(layout, depth, row_idx, col_idx) :
+					seat === '#' ? emptyCycle(layout, depth, count, row_idx, col_idx) :
 					seat
 
 				mutateCount+= newSeat == seat ? 0 : 1;
@@ -57,18 +83,25 @@ print(lines)
 //print(cycle(cycle(cycle(cycle(cycle(lines).newLayout).newLayout).newLayout).newLayout).newLayout)
 //print(cycle(cycle(cycle(cycle(cycle(cycle(lines).newLayout).newLayout).newLayout).newLayout).newLayout).newLayout)
 
-var current = lines
-do {
-	var answer = cycle(current)
-	current = answer.newLayout
-} while ((answer.mutateCount > 0))
 
-console.log({answer})
-print(answer.newLayout)
+function go (depth, count) {
+	var current = lines
+	do {
+		var answer = cycle(current, depth, count)
+		current = answer.newLayout
+		print(current)
+	} while ((answer.mutateCount > 0))
+	return answer
+}
+
+//const answer = go(1, 4)
+//console.log({answer})
+//print(answer.newLayout)
 
 
 const occupiedCount = (layout) => 
 	layout.reduce((memo, row) => row.reduce((memo, seat) => memo + (seat === '#' ? 1 : 0), memo), 0)
 
 
-console.log('Part1', occupiedCount(answer.newLayout))
+console.log('Part1', occupiedCount(go(1, 4).newLayout))
+console.log('Part2', occupiedCount(go(100000, 5).newLayout))
